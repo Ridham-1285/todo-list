7aac4de7fbf04b606ff8509ba6ed12ed47897640
Ridham-1285 <ridhamgoti05@gmail.com>
2025-05-01 15:59:30 +0530

    Add files via upload

---
+++ to-do list/script.js
@@ -0,0 +1,45 @@
+let inputbox = document.getElementById("input-box");
+let listcontainer = document.getElementById("list-container")
+
+function addtask(){
+    if(inputbox.value === ''){
+        alert("pleast enter the task!")
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputbox.value;
+        listcontainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputbox.value = '';
+    savedata();
+}
+
+listcontainer.addEventListener("click",function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        savedata();
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        savedata();
+    }
+},false);
+
+inputbox.addEventListener("keypress",function(ridham){
+    if(ridham.key === "Enter"){
+        ridham.preventDefault();
+        addtask();
+    }
+});
+
+function savedata(){
+    localStorage.setItem("data",listcontainer.innerHTML)
+}
+
+function showlist(){
+    listcontainer.innerHTML = localStorage.getItem("data");
+}
+showlist();
